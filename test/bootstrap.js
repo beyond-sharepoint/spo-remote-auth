a8b2a2306d122a4d7ca85c8832f5dac5555ba91f
Oceanswave <sean.mclellan@sixconcepts.com>
2017-02-02 20:09:11 -0500

    Adding nock record improvements
    Keeping nock at 8.0.0 as later versions seem to eat multiple Set-Cookie headers :(

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -43,7 +43,7 @@ let postProcessNockFixture = function (fixturePath) {
 
             if (_.startsWith(def.path, "/_api/web/")) {
                 def.options = def.options || {};
-                def.options.filteringRequestBody = function() {
+                def.options.filteringRequestBody = function () {
                     return "*";
                 };
             }
@@ -78,7 +78,7 @@ before(function () {
         nock.recorder.rec({
             output_objects: true,
             dont_print: true,
-            enable_reqheaders_recording: true
+            //enable_reqheaders_recording: true
         });
     }
 
@@ -92,6 +92,122 @@ after(function () {
         mkdirp.sync(path.join(__dirname, "tmp"));
 
         let nockCallObjects = nock.recorder.play();
+
+        let regExpEscape = function (s) {
+            return String(s).replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').
+                replace(/\x08/g, '\\x08');
+        };
+
+        for (let callObject of nockCallObjects) {
+            if (callObject.scope === testSettings.valid.url + ":443") {
+                callObject.scope = "https://tenant.sharepoint.com:443";
+
+                if (callObject.body)
+                    callObject.body = "*";
+            }
+            else if (callObject.scope === "https://login.microsoftonline.com:443") {
+                if (callObject.body) {
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.valid.username, "gm")), "{{{valid.username}}}");
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.valid.password, "gm")), "{{{valid.password}}}");
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.valid.url, "gm")), "{{{valid.url}}}");
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.invalid.username, "gm")), "{{{invalid.username}}}");
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.invalid.password, "gm")), "{{{invalid.password}}}");
+                    callObject.body = callObject.body.replace(new RegExp(regExpEscape(testSettings.invalid.url, "gm")), "{{{invalid.url}}}");
+                }
+            }
+
+            //Rawheaders nonsense.
+            // if (callObject.rawHeaders) {
+            //     callObject.headers = {};
+
+            //     for (let i = 0; i < callObject.rawHeaders.length; i += 2) {
+            //         let key = callObject.rawHeaders[i].toLowerCase();
+            //         //Allow for multiple cookies, such as Set-Cookie
+            //         if (callObject.headers[key]) {
+            //             if (_.isArray(callObject.headers[key])) {
+            //                 callObject.headers[key].push(callObject.rawHeaders[i + 1]);
+            //             }
+            //             else {
+            //                 let tmp = callObject.headers[key];
+            //                 callObject.headers[key] = [];
+
+            //                 callObject.headers[key].push(tmp);
+            //                 callObject.headers[key].push(callObject.rawHeaders[i + 1]);
+            //             }
+            //         }
+            //         else {
+            //             callObject.headers[key] = callObject.rawHeaders[i + 1];
+            //         }
+            //     }
+            //     delete callObject.rawHeaders;
+            // }
+
+            if (callObject.headers) {
+                for (let header in callObject.headers) {
+                    let headerLower = header.toLowerCase();
+                    switch (headerLower) {
+                        case "expires":
+                        case "last-modified":
+                        case "date":
+                            delete callObject.headers[header];
+                            break;
+                        case "x-requestdigest":
+                            callObject.headers[header] = "0x12345,{{{currentDate}}}";
+                            break;
+                        case "set-cookie":
+                            if (_.isArray(callObject.headers[header])) {
+                                for(let i = 0; i < callObject.headers[header].length; i++) {
+                                    let subHeader = callObject.headers[header][i];
+                                    subHeader = subHeader.replace(new RegExp("rtFa=.*?;", "g"), "rtFa=12345;");
+                                    subHeader = subHeader.replace(new RegExp("FedAuth=.*?;", "g"), "FedAuth=12345;");
+                                    callObject.headers[header][i] = subHeader;
+                                };
+                            } else {
+                                callObject.headers[header] = callObject.headers[header].replace(new RegExp("rtFa=.*?;", "g"), "rtFa=12345;");
+                                callObject.headers[header] = callObject.headers[header].replace(new RegExp("FedAuth=.*?;", "g"), "FedAuth=12345;");
+                            }
+                            break;
+                    }
+                }
+
+            //     //The flip-side of the rawheaders nonsense.
+            //     callObject.rawHeaders = [];
+            //     for (let header in callObject.headers) {
+            //         if (_.isArray(callObject.headers[header])) {
+            //             for(let subHeader of callObject.headers[header]) {
+            //                 callObject.rawHeaders.push(header);
+            //                 callObject.rawHeaders.push(subHeader);
+            //             }
+            //         }
+            //         else {
+            //             callObject.rawHeaders.push(header);
+            //             callObject.rawHeaders.push(callObject.headers[header]);
+            //         }
+            //     }
+            //     delete callObject.headers;
+            }
+
+            if (callObject.response) {
+                let formDigestValue = _.get(callObject.response, "d.GetContextWebInformation.FormDigestValue");
+                if (formDigestValue) {
+                    _.set(callObject.response, "d.GetContextWebInformation.FormDigestValue", "0x12345,{{{currentDate}}}");
+                }
+
+                let timeCreated = _.get(callObject.response, "d.TimeCreated")
+                if (timeCreated) {
+                    _.set(callObject.response, "d.TimeCreated", "{{{currentDate}}}");
+                }
+
+                let timeLastModified = _.get(callObject.response, "d.TimeLastModified")
+                if (timeCreated) {
+                    _.set(callObject.response, "d.TimeLastModified", "{{{currentDate}}}");
+                }
+
+                let strResponse = JSON.stringify(callObject.response);
+                strResponse = strResponse.replace(new RegExp(testSettings.valid.url, "g"), "{{{valid.url}}}");
+                callObject.response = JSON.parse(strResponse);
+            }
+        }
 
         fs.writeFileSync(path.join(__dirname, "tmp", argv.recordOutput), JSON.stringify(nockCallObjects, null, 2));
     }
